5543e842a65ab1263a40b8d6c28eb7d85685ec58
Hongchray <songhongchray@gmail.com>
2025-08-29 14:54:40 +0700

    fix(error type)

--- src/app/admin/(auth)/forgot-password/verify-otp/page.tsx
+++ src/app/admin/(auth)/forgot-password/verify-otp/page.tsx
@@ -1,9 +1,12 @@
 import VerifyOtp from "@/components/auth/forgot-verify-otp";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <div className="min-h-screen bg-background flex items-center justify-center">
-      <VerifyOtp />
+      <Suspense fallback={<div>Loading...</div>}>
+        <VerifyOtp />
+      </Suspense>
     </div>
   );
 }
